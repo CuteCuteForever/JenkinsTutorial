b5bd1ab9edafe80695e38e49657986c83dc1c783
cutecute <you@example.com>
2024-03-31 19:06:59 +0800

    Adding in apache library

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (StringUtils.equals("Hello", "Hello")) {
+            System.out.println("Hello world!");
+        } else {
+            System.out.println("Nothing to hello");
+        }
+
     }
 }
